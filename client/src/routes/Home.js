5cddea0a263c79b79276f32c982d3856f1ed1b1e
rayhook <koohyar@gmail.com>
2021-12-09 18:14:39 +0100

    Remove tailwind styling in favor of bulma

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -5,9 +5,7 @@ import { RecipeContextProvider } from "../context/RecipeContext";
 function Home() {
   return (
     <RecipeContextProvider>
-      <div className="m-0 p-0 min-h-screen font-playfair bg-gray-200">
-        <RecipeList />
-      </div>
+      <RecipeList />
     </RecipeContextProvider>
   );
 }
