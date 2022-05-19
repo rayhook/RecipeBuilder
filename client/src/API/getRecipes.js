c843771b757060e44c692ed3d2740a88be54ba36
rayhook <koohyar@gmail.com>
2022-05-19 10:50:58 +0200

    Update API endpoint from heroku to render.com

--- client/src/API/getRecipes.js
+++ client/src/API/getRecipes.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const RecipeAPI = axios.create({
-  baseURL: "https://recipe-builder-rayhook.herokuapp.com/"
+  baseURL: "https://recipe-builder-backend.onrender.com"
 });
 
 export default RecipeAPI;
